0159c8f9cdc3c6b341873875ae8a590f9dd8b1af
Max <max@rbcmail.ru>
2014-09-24 11:49:43 +0400

    Add clock to XP bar renderer

--- src/ExileHUD/XPHRenderer.cs
+++ src/ExileHUD/XPHRenderer.cs
@@ -56,23 +56,24 @@ namespace ExileHUD
 				this.hasStarted = true;
 				return;
 			}
-			if ((DateTime.Now - this.lastCalcTime).TotalSeconds > 1.0)
+			DateTime dtNow = DateTime.Now;
+			if ((dtNow - this.lastCalcTime).TotalSeconds > 1.0)
 			{
-				long num = this.poe.Player.GetComponent<Player>().XP - this.startXp;
-				float num2 = (float)((double)num / (DateTime.Now - this.startTime).TotalHours);
-				if ((double)num2 > 1000000.0)
+				long currentExp = this.poe.Player.GetComponent<Player>().XP - this.startXp;
+				float expRate = (float)((double)currentExp / (dtNow - this.startTime).TotalHours);
+				if ((double)expRate > 1000000.0)
 				{
-					this.curDisplayString = ((double)num2 / 1000000.0).ToString("0.00") + "M XP/h";
+					this.curDisplayString = ((double)expRate / 1000000.0).ToString("0.00") + "M XP/h";
 				}
 				else
 				{
-					if ((double)num2 > 1000.0)
+					if ((double)expRate > 1000.0)
 					{
-						this.curDisplayString = ((double)num2 / 1000.0).ToString("0.00") + "K XP/h";
+						this.curDisplayString = ((double)expRate / 1000.0).ToString("0.00") + "K XP/h";
 					}
 					else
 					{
-						this.curDisplayString = num2.ToString("0.00") + " XP/h";
+						this.curDisplayString = expRate.ToString("0.00") + " XP/h";
 					}
 				}
 				int level = this.poe.Player.GetComponent<Player>().Level;
@@ -80,38 +81,34 @@ namespace ExileHUD
 				{
 					return;
 				}
-				long num3 = (long)((ulong)Constants.PlayerXpLevels[level + 1] - (ulong)this.poe.Player.GetComponent<Player>().XP);
-				if (num2 > 1f)
+				ulong expRemaining = (ulong)Constants.PlayerXpLevels[level + 1] - (ulong)this.poe.Player.GetComponent<Player>().XP;
+				if (expRate > 1f)
 				{
-					int num4 = (int)((float)num3 / num2 * 3600f);
+					int num4 = (int)((float)expRemaining / expRate * 3600f);
 					int num5 = num4 / 60;
 					int num6 = num5 / 60;
-					this.curTimeLeftString = string.Concat(new object[]
-					{
-						num6,
-						"h ",
-						num5 % 60,
-						"m ",
-						num4 % 60,
-						"s until level up"
-					});
+					this.curTimeLeftString = string.Concat(num6, "h ", num5 % 60, "m ", num4 % 60, "s until level up" );
 				}
-				this.lastCalcTime = DateTime.Now;
+				this.lastCalcTime = dtNow;
 			}
-			int @int = Settings.GetInt("XphDisplay.FontSize");
-			int int2 = Settings.GetInt("XphDisplay.BgAlpha");
+
+			int fontSize = Settings.GetInt("XphDisplay.FontSize");
+			int bgAlpha = Settings.GetInt("XphDisplay.BgAlpha");
+
 			Rect clientRect = this.poe.Internal.IngameState.IngameUi.Minimap.SmallMinimap.GetClientRect();
 			Vec2 vec = new Vec2(clientRect.X - 10, clientRect.Y + 5);
 			int num7 = 0;
-			Vec2 vec2 = rc.AddTextWithHeight(new Vec2(vec.X, vec.Y), this.curDisplayString, Color.White, @int, DrawTextFormat.Right);
+			Vec2 vec2 = rc.AddTextWithHeight(new Vec2(vec.X, vec.Y), this.curDisplayString, Color.White, fontSize, DrawTextFormat.Right);
 			num7 += vec2.Y;
-			Vec2 vec3 = rc.AddTextWithHeight(new Vec2(vec.X, vec.Y + num7), this.curTimeLeftString, Color.White, @int, DrawTextFormat.Right);
+			Vec2 vec3 = rc.AddTextWithHeight(new Vec2(vec.X, vec.Y + num7), this.curTimeLeftString, Color.White, fontSize, DrawTextFormat.Right);
 			num7 += vec3.Y;
 			int val = Math.Max(vec2.X, vec3.X) + 10;
 			int num8 = Math.Max(val, Math.Max(clientRect.W, this.overlay.PreloadAlert.Bounds.W));
 			Rect rect = new Rect(vec.X - num8 + 5, vec.Y - 5, num8, num7 + 10);
 			this.Bounds = rect;
-			rc.AddBox(rect, Color.FromArgb(int2, 1, 1, 1));
+
+			rc.AddTextWithHeight(new Vec2(rect.X - num8, rect.Y), dtNow.ToShortTimeString(), Color.White, fontSize, DrawTextFormat.Left);
+			rc.AddBox(rect, Color.FromArgb(bgAlpha, 1, 1, 1));
 		}
 	}
 }
